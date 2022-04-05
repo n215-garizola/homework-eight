3d6884ecc7c3719ffb7a6f4df24937de367f8242
garizola <garizolabusiness@gmail.com>
2022-04-05 07:28:43 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#homework-eight
+
+##For homework 8 we must create a page that loads pages through jQuery
+##Only use one page to load content
+##All pages must have full content
+
+https://in-info-web4.informatics.iupui.edu/~garizola/homework-eight/
---
+++ app/app.js
@@ -0,0 +1,256 @@
+let homeContent = `<div class="hero"><h1>HOME</h1></div>
+<div class="content">
+
+
+  
+  <div class="products">
+    <div class="product">
+      <div class="product-image"></div>
+      <div class="product-description">
+        <h4>The Taxi Driver</h4>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Labore maxime id non ipsam, inventore commodi facere!
+          Assumenda est dolorum eius aspernatur fuga modi, sed enim
+          accusamus mollitia quas quidem maxime!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Labore maxime id non ipsam, inventore commodi facere!
+          Assumenda est dolorum eius aspernatur fuga modi, sed enim
+          accusamus mollitia quas quidem maxime!
+        </p>
+      </div>
+    </div>
+
+    <div class="bigImage"></div>
+
+
+    <div class="product">
+      
+      <div class="product-description">
+        <h4>The Big City</h4>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Labore maxime id non ipsam, inventore commodi facere!
+          Assumenda est dolorum eius aspernatur fuga modi, sed enim
+          accusamus mollitia quas quidem maxime!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Labore maxime id non ipsam, inventore commodi facere!
+          Assumenda est dolorum eius aspernatur fuga modi, sed enim
+          accusamus mollitia quas quidem maxime!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Labore maxime id non ipsam, inventore commodi facere!
+          Assumenda est dolorum eius aspernatur fuga modi, sed enim
+          accusamus mollitia quas quidem maxime!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Labore maxime id non ipsam, inventore commodi facere!
+          Assumenda est dolorum eius aspernatur fuga modi, sed enim
+          accusamus mollitia quas quidem maxime!
+        </p>
+      </div>
+    </div>
+
+    <div class="bigImage1"></div>
+  </div>
+</div>;`
+let aboutContent = `<div class="hero"><h1>ABOUT</h1></div>
+<div class="content">
+
+  
+  <div class="products">
+    <div class="product">
+      <div class="product-image"></div>
+      <div class="product-description">
+        <h4>About Travis</h4>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Labore maxime id non ipsam, inventore commodi facere!
+          Assumenda est dolorum eius aspernatur fuga modi, sed enim
+          accusamus mollitia quas quidem maxime!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Labore maxime id non ipsam, inventore commodi facere!
+          Assumenda est dolorum eius aspernatur fuga modi, sed enim
+          accusamus mollitia quas quidem maxime!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Labore maxime id non ipsam, inventore commodi facere!
+          Assumenda est dolorum eius aspernatur fuga modi, sed enim
+          accusamus mollitia quas quidem maxime!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Labore maxime id non ipsam, inventore commodi facere!
+          Assumenda est dolorum eius aspernatur fuga modi, sed enim
+          accusamus mollitia quas quidem maxime!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Labore maxime id non ipsam, inventore commodi facere!
+          Assumenda est dolorum eius aspernatur fuga modi, sed enim
+          accusamus mollitia quas quidem maxime!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Labore maxime id non ipsam, inventore commodi facere!
+          Assumenda est dolorum eius aspernatur fuga modi, sed enim
+          accusamus mollitia quas quidem maxime!
+        </p>
+      </div>
+    </div>
+
+  <div class="image-big"></div>
+
+    <div class="product">
+      <div class="product-image1"></div>
+      <div class="product-description">
+        <h4>Big City</h4>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Labore maxime id non ipsam, inventore commodi facere!
+          Assumenda est dolorum eius aspernatur fuga modi, sed enim
+          accusamus mollitia quas quidem maxime!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Labore maxime id non ipsam, inventore commodi facere!
+          Assumenda est dolorum eius aspernatur fuga modi, sed enim
+          accusamus mollitia quas quidem maxime!
+        </p>
+      </div>
+    </div>
+  </div>
+
+</div>;`
+let productsContent = `<div class="hero"><h1>PRODUCTS</h1></div>
+<div class="content">
+  <div class="products">
+    <div class="product">
+      <div class="product-image1"></div>
+      <div class="product-description">
+        <h4>Big City</h4>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Labore maxime id non ipsam, inventore commodi facere!
+          Assumenda est dolorum eius aspernatur fuga modi, sed enim
+          accusamus mollitia quas quidem maxime!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Labore maxime id non ipsam, inventore commodi facere!
+          Assumenda est dolorum eius aspernatur fuga modi, sed enim
+          accusamus mollitia quas quidem maxime!
+        </p>
+      </div>
+    </div>
+    <div class="imagebig"></div>
+    
+
+    <div class="product">
+      <div class="product-image"></div>
+      <div class="product-description">
+        <h4>Big City Mood</h4>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Labore maxime id non ipsam, inventore commodi facere!
+          Assumenda est dolorum eius aspernatur fuga modi, sed enim
+          accusamus mollitia quas quidem maxime!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Labore maxime id non ipsam, inventore commodi facere!
+          Assumenda est dolorum eius aspernatur fuga modi, sed enim
+          accusamus mollitia quas quidem maxime!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Labore maxime id non ipsam, inventore commodi facere!
+          Assumenda est dolorum eius aspernatur fuga modi, sed enim
+          accusamus mollitia quas quidem maxime!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Labore maxime id non ipsam, inventore commodi facere!
+          Assumenda est dolorum eius aspernatur fuga modi, sed enim
+          accusamus mollitia quas quidem maxime!
+        </p>
+      </div>
+    </div>
+  </div>
+</div>;`
+let contactContent = `<div class="hero"><h1>CONTACT</h1></div>
+<div class="content">
+  
+
+  
+  <div class="products">
+    <div class="product">
+      <div class="product-image"></div>
+      <div class="product-description">
+        <h4>How to contact: </h4>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Labore maxime id non ipsam, inventore commodi facere!
+          Assumenda est dolorum eius aspernatur fuga modi, sed enim
+          accusamus mollitia quas quidem maxime!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Labore maxime id non ipsam, inventore commodi facere!
+          Assumenda est dolorum eius aspernatur fuga modi, sed enim
+          accusamus mollitia quas quidem maxime!
+        </p>
+      </div>
+    </div>
+
+    <div class="imagebig1"></div>
+
+    <div class="product">
+      <div class="product-description">
+        <h4>Contact Below: </h4>
+        
+        <p>
+         taxidriver@gmail.com
+        </p>
+        <p>
+         111-222-3333
+        </p>
+      </div>
+    </div>
+  </div>
+</div>;`
+function initListeners() {
+    $("nav a ").click(function (e){
+        let btnID = e.currentTarget.id;
+        loadPageContent(btnID);
+        
+
+
+    })
+}
+
+function loadPageContent(pageName) {
+    let contentID = pageName + "Content";
+    $("#app").html(eval(contentID));
+}
+
+
+function initApp() {
+    initListeners();
+}
+
+
+
+$(document).ready(function(){
+    loadPageContent("home");
+    initApp();
+});
